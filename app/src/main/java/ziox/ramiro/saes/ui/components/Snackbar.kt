3d6615e29bfd5812c41c035d5db4e9f2a979d4e1
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2021-07-05 15:39:10 -0500

    :lipstick: Color danger en Snackbar

--- app/src/main/java/ziox/ramiro/saes/ui/components/Snackbar.kt
+++ app/src/main/java/ziox/ramiro/saes/ui/components/Snackbar.kt
@@ -1,14 +1,11 @@
 package ziox.ramiro.saes.ui.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +17,7 @@ import ziox.ramiro.saes.ui.theme.getCurrentTheme
 fun ErrorSnackbar(
     text: String = "Snackbar"
 ) = Snackbar(
-    backgroundColor = getCurrentTheme().dangerColor,
+    backgroundColor = getCurrentTheme().danger,
     shape = RoundedCornerShape(16.dp)
 ) {
     ListItem(
