882ba7789605d67783bdfd086393cad59324dc54
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2025-07-04 23:18:09 -0600

    feat: migrate sdk 30 to 31

--- app/src/main/java/ziox/ramiro/saes/data/models/Notification.kt
+++ app/src/main/java/ziox/ramiro/saes/data/models/Notification.kt
@@ -67,7 +67,7 @@ class NotificationBuilder(val context: Context){
 
     @SuppressLint("UnspecifiedImmutableFlag")
     fun setPendingIntent(intent: Intent) = this.also {
-        pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
     }
 
     fun buildAndNotify(notificationId: Int) {
